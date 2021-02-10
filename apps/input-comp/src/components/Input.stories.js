33a51ad56144e9b5d9273bded56052a973ba3104
Justin Walsh <justinw@mortgage365.com>
2021-02-09 23:14:54 -0700

    feature(input-comp): Input Component ✔️

--- apps/input-comp/src/components/Input.stories.js
+++ apps/input-comp/src/components/Input.stories.js
@@ -8,7 +8,18 @@ export default {
   component: Input,
   argTypes: {
     label: { control: 'text' },
-    placeholder: { control: 'text' }
+    placeholder: { control: 'text' },
+    disabled: { control: 'boolean' },
+    error: { control: 'boolean' },
+    size: {
+      control: {
+        type: 'select',
+        options: [
+          'sm',
+          'md'
+        ]
+      }
+    }
   }
 };
 
@@ -18,4 +29,65 @@ export const Primary = Template.bind({});
 Primary.args = {
   label: 'Label',
   placeholder: 'Placeholder',
-}
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  ...Primary.args,
+  error: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Primary.args,
+  disabled: true,
+};
+
+export const HelperText = Template.bind({});
+HelperText.args = {
+  ...Primary.args,
+  helperText: 'Some interesting text',
+};
+
+export const HelperTextError = Template.bind({});
+HelperTextError.args = {
+  ...HelperText.args,
+  error: true,
+};
+
+export const StartIcon = Template.bind({});
+StartIcon.args = {
+  ...Primary.args,
+  startIcon: 'local_phone',
+};
+
+export const EndIcon = Template.bind({});
+  EndIcon.args = {
+  ...Primary.args,
+  endIcon: 'lock',
+};
+
+export const Value = Template.bind({});
+  Value.args = {
+  ...Primary.args,
+  value: 'Text',
+};
+
+export const SizeSmall = Template.bind({});
+  SizeSmall.args = {
+  ...Primary.args,
+  size: 'sm',
+};
+
+export const FullWidth = Template.bind({});
+  FullWidth.args = {
+  ...Primary.args,
+  fullWidth: true,
+};
+
+export const Multiline = Template.bind({});
+  Multiline.args = {
+  ...Primary.args,
+  multiline: true,
+  row: 4,
+};
--- apps/input-comp/src/components/Input.tsx
+++ apps/input-comp/src/components/Input.tsx
@@ -1,16 +1,149 @@
 import React, { useState } from 'react'
+import styled, { css } from "styled-components";
+import Icon from '@material-ui/core/Icon';
 
-type InputProps = {
+type InputProps = Partial<{
   label: string,
-  placeholder?: string
+  placeholder: string,
+  helperText: string,
+  value: string,
+  error: boolean,
+  disabled: boolean,
+  size: 'sm' | 'md',
+  startIcon: string,
+  endIcon: string,
+  fullWidth: boolean,
+  multiline: boolean,
+  row: number,
+}>;
+
+type StyleProps = {
+  error: boolean,
+  height: 'sm' | 'md',
+  fullWidth: boolean,
+  startIcon?: string,
+  endIcon?: string,
+  row?: number,
 }
 
-export const Input = ({ label, placeholder, ...props}: InputProps) => {
-  const [value, setValue] = useState<string>('');
+const colors = {
+  default: '#828282',
+  focus: '#2962ff',
+  error: '#D32f2f'
+}
+
+const Label = styled.label<StyleProps>`
+  color: ${props => props.error ? colors.error : '#333333'};
+  &:focus-within {
+    color: ${props => props.error ? colors.error : colors.focus};
+  }
+  position: relative;
+  .start-icon {
+    position: absolute;
+    bottom: -4px;
+    left: 12px;
+    color: #828282;
+  }
+  .end-icon {
+    position: absolute;
+    bottom: -4px;
+    right: 12px;
+    color: #828282;
+  }
+`;
+
+const LabelText = styled.p`
+  font-family: 'Noto Sans JP', sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+  margin-bottom: .2rem;
+`;
+
+const Info = styled.p<StyleProps>`
+  ${Label};
+  font-size: 10px;
+  margin-top: .1rem;
+  color: ${props => props.error ? colors.error : '#828282'};
+`;
+
+const StyledInput = styled.input<StyleProps>`
+  font-family: 'Noto Sans JP', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: ${props => props.height === 'sm' ? '40px' : '56px'};
+  height: ${props => props.height === 'sm' ? '40px' : '56px'};
+  min-width: 200px;
+  width: ${props => props.fullWidth ? '100%' : 'inherit'};
+  box-sizing: border-box;
+  border: 1px solid ${props => props.error ? colors.error : colors.default};
+  border-radius: 8px;
+  padding: 0px 12px;
+  outline: none;
+  &:disabled {
+    background-color: #F2F2F2;
+  };
+  &:hover {
+    border-color: #333333;
+  };
+  &:focus {
+    border-color: ${props => props.error ? colors.error : colors.focus};
+  };
+
+  ${props => props.startIcon && css`
+    padding-left: 45px;
+  `};
+
+  ${props => props.endIcon && css`
+    padding-right: 45px;
+  `};
+`;
+
+const StyledTextArea = styled.textarea<StyleProps>`
+  font-family: 'Noto Sans JP', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  height: ${props => props.row * 37 + 'px'};
+  min-width: 200px;
+  min-height: 56px;
+  width: ${props => props.fullWidth ? '100%' : 'inherit'};
+  box-sizing: border-box;
+  border: 1px solid ${props => props.error ? colors.error : colors.default};
+  border-radius: 8px;
+  padding: 17px 12px;
+  outline: none;
+  &:disabled {
+    background-color: #F2F2F2;
+  };
+  &:hover {
+    border-color: #333333;
+  };
+  &:focus {
+    border-color: ${props => props.error ? colors.error : colors.focus};
+  };
+`;
+
+export const Input = ({ label, placeholder, helperText, value, multiline, ...props}: InputProps) => {
+  const [inputVal, setInputVal] = useState<string>(value || '');
+  
+  const { error, size, fullWidth, startIcon, endIcon, row } = props;
+  const style:StyleProps = { error, height: size, fullWidth, startIcon, endIcon, row: row || 1};
+
+  const StartIcon = startIcon ? <Icon className="start-icon">{startIcon}</Icon> : null;
+  const EndIcon = endIcon ? <Icon className="end-icon">{endIcon}</Icon> : null;
+  const StyledTextInput = multiline ?
+    <StyledTextArea id="input" value={inputVal} placeholder={placeholder} onChange={(e) => setInputVal(e.target.value)} disabled={props.disabled} {...style} /> :
+    <StyledInput id="input" type="text" value={inputVal} placeholder={placeholder} onChange={(e) => setInputVal(e.target.value)} disabled={props.disabled} {...style} />;
+
   return (
-    <label htmlFor="input">
-      <p>{label}</p>
-      <input id="input" type="text" value={value} placeholder={placeholder} onChange={(e) => setValue(e.target.value)} />
-    </label>
+    <>
+      <Label htmlFor="input" {...style}>
+        <LabelText>{label}</LabelText>
+          {StyledTextInput}
+          {StartIcon}
+          {EndIcon}
+      </Label>
+      <Info {...style}>{helperText}</Info>     
+    </>
   );
 }
